ff958ce7b4a8e2629675d4a9db508f60b66046b3
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-11-28 10:53:15 -0600

    commit tree a806ff2718c57592acb927f575c33b2e01935173

--- src/Parent.js
+++ src/Parent.js
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
+// Parent is the component : color is the state variable 
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF")
 
+function handleChangeColor(newChildColor){
+  const newRandomColor = getRandomColor();
+  setColor(newRandomColor); 
+  setChildrenColor(newChildColor);
+}
+
+//handleChangeColor() is passed as a state changing function as a ----PROP---- to both Child component. This allows the handleChangeColor() to run when Child component is CLICKED 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
